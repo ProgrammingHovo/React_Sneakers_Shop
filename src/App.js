8b0f5becc80b88d473a6da4986970e0596a836f8
Hovhannes Ghaltakhchyan <hovo.program@gmail.com>
2022-09-10 14:07:58 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <header>
+          <div className="headerLeft">
+
+            <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+
+            <div className="headerInfo">
+              <h3>SNEAKERS SHOP</h3>
+              <p>Магазин лучших кроссовок</p>
+            </div>
+
+          </div>
+
+          <div className="headerRight">
+            <ul>
+
+              <li>
+                <img className="basket_img" width={18} height={18} src="/img/cart.svg" alt="basket"/>
+                <span>0 руб.</span>
+              </li>
+
+              {/* <li>
+                <img width={18} height={18} src="/img/favorite.svg" alt="favorite"/>
+              </li> */}
+
+              <li className="user_img">
+                <img width={18} height={18} src="/img/user.svg" alt="user"/>
+              </li>
+
+            </ul>
+          </div>
+
+        </header>
+
+        <div className="content">
+
+          <h1>Все кроссовки</h1>
+
+          <div className="cards">
+
+          <div className="card">  
+
+            <img className="sneakerImg" width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneaker" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+
+            <div className="cardBottom">
+              <div className="priceInfo">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+
+              <button className="plusButton">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+              </button>
+            </div>
+
+            </div>
+
+
+            <div className="card">  
+
+            <img className="sneakerImg" width={133} height={112} src="/img/sneakers/2.jpg" alt="Sneaker" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+
+            <div className="cardBottom">
+              <div className="priceInfo">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+
+              <button className="plusButton">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+              </button>
+            </div>
+
+            </div>
+
+
+            <div className="card">  
+
+            <img className="sneakerImg" width={133} height={112} src="/img/sneakers/3.jpg" alt="Sneaker" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+
+            <div className="cardBottom">
+              <div className="priceInfo">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+
+              <button className="plusButton">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+              </button>
+            </div>
+
+            </div>
+
+
+            <div className="card">  
+
+            <img className="sneakerImg" width={133} height={112} src="/img/sneakers/4.jpg" alt="Sneaker" />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+
+            <div className="cardBottom">
+              <div className="priceInfo">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+
+              <button className="plusButton">
+                <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+              </button>
+            </div>
+
+            </div>
+
+          </div>
+
+        </div>
     </div>
   );
 }
